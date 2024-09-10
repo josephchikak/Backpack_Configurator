f8d00176833ef563fbec57d418e568d2ef090285
raytheboffin <111377374+josephchikak@users.noreply.github.com>
2024-09-10 16:49:32 +0100

    added qr code

--- src/utility/useStore.jsx
+++ src/utility/useStore.jsx
@@ -4,9 +4,11 @@ import { create } from 'zustand'
         material: 'leather',
         color:'#d07a3c',
         metalColor: '#010203',
+        AR: false,
         updateMaterial: (mat) =>  set((state) => ({material: mat })),
         updateColor:  (color) =>  set((state) => ({color: color})),
         updateMetalColor:  (metalColor) =>  set((state) => ({metalColor: metalColor})),
+        updateAR: (state) => set((state) => ({AR: !state.AR}))
 
     }))
 
